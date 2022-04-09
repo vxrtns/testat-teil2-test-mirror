259f188c8d74a600cf32b413c5e7c835802b154f
vxrtns <vxrtns@protonmail.com>
2022-04-09 15:45:26 +0200

    Behebe Konfigurationsfehler

--- app/development/index.php
+++ app/development/index.php
@@ -8,7 +8,7 @@
     <h1>Hello, World!</h1>
     <p>Die aktuelle MariaDB-Version ist:</p>
     <?php
-    $pdo = new PDO('mysql:dbname=tutorial;host=mysql', 'tutorial', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $pdo = new PDO('mysql:dbname=webdev;host=mysql', 'webdev', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
      $query = $pdo->query('SHOW VARIABLES like "version"');
 
