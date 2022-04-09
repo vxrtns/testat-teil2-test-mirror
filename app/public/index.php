2709e8b05bdf877a0b2f70bbccb0c7dc4946c797
vxrtns <vxrtns@protonmail.com>
2022-04-09 14:45:19 +0200

    Füge Basis-Entwicklungsumgebung hinzu

---
+++ app/public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Test</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+    <p>Die aktuelle MariaDB-Version ist:</p>
+    <?php
+    $pdo = new PDO('mysql:dbname=tutorial;host=mysql', 'tutorial', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+     $query = $pdo->query('SHOW VARIABLES like "version"');
+
+    $row = $query->fetch();
+
+    echo 'MySQL version: ' . $row['Value'];
+    ?>
+  </body>
+</html>
